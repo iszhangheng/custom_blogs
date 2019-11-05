31dc5ed9f00ec96a34691d6aa29f03c3c053ac02
iszhangheng <iszhangheng@foxmail.com>
2019-11-05 13:44:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
-<!--
- * @Author: iszhangheng
- * @Date: 2019-07-18 13:52:51
- * @LastEditors: iszhangheng
- * @LastEditTime: 2019-07-18 13:52:51
- * @Description: file content
- -->
+
 <template>
   <div id="app">
     <router-view />
